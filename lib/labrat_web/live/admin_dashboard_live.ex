cdde329a508006a337aac31879f97b32e62f2305
sirlionfish <justin.ke@deemoney.com>
2022-10-25 16:37:53 +0700

    user & admin dashboard

---
+++ lib/labrat_web/live/admin_dashboard_live.ex
@@ -0,0 +1,18 @@
+defmodule LabratWeb.AdminDashboardLive do
+  use LabratWeb, :live_view
+
+  @impl true
+  def mount(_params, session, socket) do
+    socket = assign_defaults(session, socket)
+    {:ok, socket}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <section class='phx-hero'>
+      <h1>Welcome to the admin dashboard!</h1>
+    </section>
+    """
+  end
+end
