696aab83c2a63abcc6c85d5b6b46420be0acecfb
sirlionfish <justin.ke@deemoney.com>
2022-10-25 15:15:33 +0700

    new dep

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Labrat.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.6.12"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
@@ -49,7 +50,8 @@ defmodule Labrat.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:ecto_enum, "~> 1.4"}
     ]
   end
 
